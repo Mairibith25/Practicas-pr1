62a6c7587f9f758971825c63536b62a9b6e9546c
Mairibith25 <mairibithsanchez@gmail.com>
2022-01-29 00:14:14 -0400

    Suma de dos enteros.

--- Entero.c
+++ Entero.c
@@ -3,9 +3,10 @@
  
    int A=20;
    int B=5;
-   int resul=A+B;
+   int result=A+B;
     printf("La variable A es un entero, y su valor es:%d\n",A);
     printf("La variable B es un entero, y su valor es:%d\n",B);
+    printf("La suma de %d + %d es =%d\n",A,B,result);
  
  return 0;
  }
