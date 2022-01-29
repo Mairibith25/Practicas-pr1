c53eaec88aa41a8bdd9022edc2b4ce4f25a5acb3
Mairibith25 <mairibithsanchez@gmail.com>
2022-01-29 00:25:02 -0400

    Resta, Multiplicacion, y Division de los enteros A y B

--- Entero.c
+++ Entero.c
@@ -3,10 +3,16 @@
  
    int A=20;
    int B=5;
-   int result=A+B;
+   int suma=A+B;
+   int resta=A-B;
+   int multi=A*B;
+   int divi=A/B;
     printf("La variable A es un entero, y su valor es:%d\n",A);
     printf("La variable B es un entero, y su valor es:%d\n",B);
-    printf("La suma de %d + %d es =%d\n",A,B,result);
+    printf("La suma de %d + %d es =%d\n",A,B,suma);
+    printf("La resta de %d - %d es =%d\n",A,B,resta);
+    printf("La multiplicacion de %d * %d es =%d\n",A,B,multi);
+    printf("La division de %d / %d es =%d\n",A,B,divi);
  
  return 0;
  }
