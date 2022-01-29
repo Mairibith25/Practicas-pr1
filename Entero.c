2333a72936ba5db45957ba83b46bcf90ab215369
Mairibith25 <mairibithsanchez@gmail.com>
2022-01-29 00:10:00 -0400

    Dos variables enteras y su valor.

---
+++ Entero.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main(){
+ 
+   int A=20;
+   int B=5;
+   int resul=A+B;
+    printf("La variable A es un entero, y su valor es:%d\n",A);
+    printf("La variable B es un entero, y su valor es:%d\n",B);
+ 
+ return 0;
+ }
